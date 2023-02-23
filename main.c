9e2983cd0c502eeb9fbba3af49227a4be33e2527
programmer <93894129+qwerty83927@users.noreply.github.com>
2023-02-23 13:39:15 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+extern int get_cursor(void);
+extern void set_cursor(int offset);
+
 unsigned char inb(unsigned short port) {
 unsigned char result;
 __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
@@ -8,5 +11,25 @@ void outb(unsigned short port, unsigned char data) {
 __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
-void main() {
+void inc_cursor() {set_cursor((get_cursor()/2)+2);}
+void dec_cursor() {set_cursor((get_cursor()/2)-2);}
+
+void putc(char c) {
+char* p = (char*)(0xb8000);
+int offset = get_cursor();
+p[offset] = c;
+p[offset+1] = 0x0f;
+inc_cursor();
+}
+
+void print(char* string) {
+int i = 0;
+while(string[i] != 0) {
+putc(string[i]);
+++i;
+}
+}
+
+void main(void) {
+print("hello world");
 }
